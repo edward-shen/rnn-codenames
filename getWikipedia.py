cd29a09c38d99edcf761fa221ca060ca89f94056
Edward Shen <6173958+edward-shen@users.noreply.github.com>
2017-10-27 01:02:10 -0400

    Initial commit

---
+++ getWikipedia.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import re#eeeeeeeeeeeeeeeeeeeeeee
+
+# Fetches our data
+wikiDataUrl = "https://en.wikipedia.org/w/api.php?action=parse&page=List_of_computer_technology_code_names&srlimit=1&format=json"
+wikiDataR = requests.get(wikiDataUrl)
+
+# Parse our data
+wikiData = json.loads(wikiDataR.text)["parse"]["text"]["*"]
+
+
+
+# Creates our regex filter for titles
+getTitles = re.compile('<b>(.*)<\/b>')
+checkNotClean = re.compile('<a(.*)>(.*)<\/a(.*)>')
+
+
+# Removes any html tag that surrond the values
+def clean(input):
+    if checkNotClean.match(input):
+        input = input.split(">", 1)[1]
+        input = input.split("<", 1)[0]
+        return input
+    else:
+        return input
+
+# Parse our titles into a list
+wikiData = getTitles.findall(wikiData)
+
+# clean up our data
+wikiData = list(map(clean, wikiData))
+
+# The last two values are "^"
+wikiData = wikiData[:-2]
+output = open("char-rnn-tensorflow-master/data/codenames/input.txt", "w")
+for item in wikiData:
+  output.write("%s\n" % item)
+
